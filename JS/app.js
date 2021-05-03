79bba7a26f0c6f6bae55089da42c341b4c5e728d
MatthewLockyer <73995041+MatthewLockyer@users.noreply.github.com>
2021-05-03 23:50:34 +0100

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -55,6 +55,8 @@ app()
 
 const heroBox = document.querySelectorAll('.heroBox')
 const heroPhotos = document.querySelectorAll('.heroIMG');
+const titleBackground = document.querySelectorAll('.gradientText')
+const heroBackground = document.querySelector('.hero')
 
 
 heroBox.forEach((box, i) => {
@@ -64,5 +66,52 @@ heroBox.forEach((box, i) => {
             photo.classList.remove('heroIMGShow')
         })
         heroPhotos[i].classList.add('heroIMGShow');
+
+        box.classList.remove('heroBoxShadow')
+
+
+        heroBox.forEach((bo) => {
+            bo.classList.remove('heroBoxShadow')
+        })
+
+        heroBox[i].classList.add('heroBoxShadow')
+
+
+        titleBackground.forEach((titleColour) => {
+            if(i === 0){
+                titleColour.style.background =  'linear-gradient('
+                + '145deg' + ', ' + "#241cba" + ', ' + '#b7159c' + ', ' + '#b58712' + ')';
+                titleColour.style.webkitBackgroundClip = "text";
+                titleColour.style.webkitTextFillColor = "transparent";
+            };
+            if(i === 1){
+                titleColour.style.background =  'linear-gradient('
+                + '145deg' + ', ' + "#08633A" + ', ' + '#098A44' + ', ' + '#90C6A8' + ')';
+                titleColour.style.webkitBackgroundClip = "text";
+                titleColour.style.webkitTextFillColor = "transparent";
+            };
+            if(i === 2){
+                titleColour.style.background =  'linear-gradient('
+                + '145deg' + ', ' + "#77191D" + ', ' + '#A03B3B' + ', ' + '#D5595E' + ')';
+                titleColour.style.webkitBackgroundClip = "text";
+                titleColour.style.webkitTextFillColor = "transparent";
+            };
+        })
+
+
+        heroBackground.classList.remove('herobckgrd1')
+        heroBackground.classList.remove('herobckgrd2')
+        heroBackground.classList.remove('herobckgrd3')
+
+        if(i === 0 ){
+            heroBackground.classList.add('herobckgrd1')
+        };
+        if(i === 1 ){
+            heroBackground.classList.add('herobckgrd2')
+        };
+        if(i === 2 ){
+            heroBackground.classList.add('herobckgrd3')
+        };
+
     })
 })
